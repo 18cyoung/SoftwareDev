5f78679a3e941c62d9931a03f1603db7420c4ac1
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-23 12:13:19 +1100

    From PyCharm. Added comments.

--- EX1DNA/Replacement.py
+++ EX1DNA/Replacement.py
@@ -1,6 +1,8 @@
 Sequence = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
+# Make sequence lower case so that replacement doesn't replace already affected letters.
 Sequence = Sequence.lower()
 
+#Replace letters with uppercase letters
 Sequence2 = Sequence.replace("a", "C")
 Sequence2 = Sequence2.replace("c", "A")
 Sequence2 = Sequence2.replace("t", "G")
