20a968dd755bf5f5bf894724d0f6932ce398747f
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-23 11:59:42 +1100

    From PyCharm. Added Part 2.

--- EX1DNA/SplicingOutIntrons.py
+++ EX1DNA/SplicingOutIntrons.py
@@ -8,3 +8,15 @@ Exon2 = '''ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGA
 TCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT'''[91:125]
 
 print(Exon1 + Exon2)
+
+### PART 2
+
+SeqLen = len(Sequence)
+Exon1Len = len(Exon1)
+Exon2Len = len(Exon2)
+
+ExonNum = Exon1Len+Exon2Len
+
+Percentage = round((ExonNum/SeqLen)*100,2)
+
+print(Percentage , "% is coding")
