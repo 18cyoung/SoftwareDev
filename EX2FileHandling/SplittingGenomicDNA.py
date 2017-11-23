083a7ce15c83d8229810a67e978bef053dc65319
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-23 12:39:38 +1100

    From PyCharm. The printed Exons.

--- EX2FileHandling/SplittingGenomicDNA.py
+++ EX2FileHandling/SplittingGenomicDNA.py
@@ -1,7 +1,18 @@
-File = open(GenomicDNA.txt)
+File = open("GenomicDNA.txt")
 DNA = File.read()
 
-Exon1 = File[0:64]
-Exon2 = File[91:]
+Exon1 = DNA[0:64]
+Exon2 = DNA[91:124]
+Intron1 = DNA[65:91]
 
 print(len(DNA))
+
+Coding = (Exon1 + "\n" + Exon2)
+
+Exons = open("CodingDNA.txt","w")
+
+Exons.write(Coding)
+
+Introns = open("NonCodingDNA.txt","w")
+
+Introns.write(Intron1)
