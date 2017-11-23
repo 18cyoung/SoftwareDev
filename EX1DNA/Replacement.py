912a0ff64031a4b39be367e1ee18771210552a11
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-23 12:12:19 +1100

    From PyCharm. Actually did the replacement.

--- EX1DNA/Replacement.py
+++ EX1DNA/Replacement.py
@@ -1,6 +1,11 @@
 Sequence = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
+Sequence = Sequence.lower()
 
-Sequence2 = Sequence.replace("T", "U")
+Sequence2 = Sequence.replace("a", "C")
+Sequence2 = Sequence2.replace("c", "A")
+Sequence2 = Sequence2.replace("t", "G")
+Sequence2 = Sequence2.replace("g", "T")
+
 
 print(Sequence2)
 
