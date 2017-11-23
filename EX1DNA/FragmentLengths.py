8351954d998722ac631557798c09bdaabd703831
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-23 12:14:08 +1100

    From PyCharm. Added comments.

--- EX1DNA/FragmentLengths.py
+++ EX1DNA/FragmentLengths.py
@@ -1,6 +1,8 @@
 Sequence = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 
+# Seperated Sequence into desired lengths
 Fragment1 = (Sequence[0:22])
 Fragment2 = (Sequence[23:56])
 
+# Prints the number of letters in each fragment
 print(len(Fragment1) , len(Fragment2))
