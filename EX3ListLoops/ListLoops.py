18ea30f8b10b6db186ee6621cb295ebe183c4088
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-27 13:55:44 +1100

    From PyCharm. Wrote list loops class example.

---
+++ EX3ListLoops/ListLoops.py
@@ -0,0 +1,9 @@
+words = []
+sentence = input('Enter a sentence: ')
+words = sentence.split(' ')
+print(words)
+
+points = []
+gamePoints = (input('Enter the points for the game played (E.g. 5,6,7): '))
+points = gamePoints.split(',')
+print(points)
