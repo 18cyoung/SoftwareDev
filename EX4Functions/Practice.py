2fa13ff5c2a4934fb87b9e81ffaa7d867630d8e1
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-11-30 14:49:09 +1100

    From PyCharm. Turned the practice into a menu and did ex 4.1.

--- EX4Functions/Practice.py
+++ EX4Functions/Practice.py
@@ -1,16 +1,62 @@
 #Create a main function that runs when this file is executed
-def main():
+def menu():
     print('Hello,')
-    print('This program will ask you for a temperature in farenheit.')
+    print('This program will help you convert measurements.')
     print('----------')
-    val = int(input('Enter a value:'))
-    ftoc(val)
+    print('''The options are:
+        1) Farenheit to Celcius
+        2) Celcius to Farenheit
+        3) Kilometres to Metres
+        4) Metres to Kilometres''')
 
 #Create a function to convert farenheit to celcius
 def ftoc(fah):
-    #fah = int(input('Enter a temperature: '))
     cel = round((fah-32)*(5/9),2)
     print(fah,'farenheit =',cel,'degrees celcius')
 
-main()
+#Create a function to convert celcius to farenheit
+def ctof(cel):
+    fah2 = round((cel2*1.8+32),2)
+    print(cel2,'farenheit =',fah2,'degrees farenheit')
+
+#Create a function to kilometres to metres
+def ktom(k):
+    m = round(k*1000,2)
+    print(k,'kilometres =',m,'metres')
+
+#Create a function to kilometres to metres
+def mtok(m):
+    k = round(m/1000,2)
+    print(m,'metres =',k,'kilometres')
+
+menu()
+
+val = 0
+while val == 0:
+    val = int(input('Please choose an option: '))
+    if val == 1:
+        fah = int(input('Enter a temperature: '))
+        ftoc(fah)
+        print('')
+        val = 0
+        menu()
+    if val == 2:
+        cel2 = int(input('Enter a temperature: '))
+        ctof(cel2)
+        print('')
+        val = 0
+        menu()
+    if val == 3:
+        k = int(input('Enter number of kilometres: '))
+        ktom(k)
+        print('')
+        val = 0
+        menu()
+    if val == 4:
+        m = int(input('Enter number of metres: '))
+        mtok(m)
+        print('')
+        val = 0
+        menu()
+
 
--- EX4Functions/aa_Content.py
+++ EX4Functions/aa_Content.py
@@ -1,1 +1,12 @@
-def aa_Content
+def aa_Content():
+    DNA = input('Please enter a protein sequence: ')
+    RC = input('Please enter an amino acid residue code: ')
+    DNA = list(DNA)
+    RCnum = DNA.count(RC)
+    DNAlen = len(DNA)
+    rcComp = round((RCnum/DNAlen)*100,2)
+    print('There is',RCnum,'of your letter(s):',RC)
+    print('There is',DNAlen,'letter(s) in your protein sequence')
+    print('The percentage composition of',RC,'is therefore',rcComp,'percent')
+
+aa_Content()
