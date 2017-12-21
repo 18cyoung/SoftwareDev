559a8f750257f491b14ac672683ceebf6ee565bb
18cyoung <18cyoung@mlmc.vic.edu.au>
2017-12-21 15:22:21 +1100

    From PyCharm. Just making sure this is properly committed to GitHub.

--- HolidayTask/DNA1.py
+++ HolidayTask/DNA1.py
@@ -11,9 +11,10 @@ Sus = DNA.find(Fragment)
 
 #Analyses if fragment is in the suspect DNA then calculate the array location and print
 if Fragment in DNA:
-    print("The DNA fragment is present in the provided sequence. It is at the location [",Sus,",",(Sus+len(Fragment)),"]")
+    print("The DNA fragment is present in the provided sequence.")
+    print("It is at the location [",Sus,",",(Sus+len(Fragment)),"]")
 else:
     print("The DNA fragment is not present in the provided sequence.")
 
 #Confirm that the found fragment is correct
-print("The fragment is:",DNA[98:102])
+print("The fragment is:",DNA[Sus:Sus+len(Fragment)])
